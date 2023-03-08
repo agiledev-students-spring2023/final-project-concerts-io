8bf383b2948b27ad4e0578c5f9b543e526c46631
mindyjwu <mhw370@nyu.edu>
2023-03-08 13:42:31 -0500

    changes to Menu bar, addition of mission statement at About page

--- front-end/src/components/Menu.js
+++ front-end/src/components/Menu.js
@@ -7,18 +7,18 @@ const Menu = () =>{
     return(
             <div className="menu-wrapper">
                 <div className='menu-box'>
-                <Link to="/about">
-                    <h2>About</h2>
-                </Link>
+                    <Link to="/about">
+                        <h2>About</h2>
+                    </Link>
                 </div>
                 <div className='menu-box'>
                     <Link to="/">
-                    <h2>Home</h2>
+                        <h2>Home</h2>
                     </Link>
                 </div>
                 <div className='menu-box'>
                     <Link to="/profile">
-                    <h2>Profile</h2>
+                        <h2>Profile</h2>
                     </Link>
                 </div>
             </div>
