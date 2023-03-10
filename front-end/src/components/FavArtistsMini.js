38256e3598846ba9e1bbb17d95c372d84617d500
liannnaa <lnpj2003@gmail.com>
2023-03-09 20:43:37 -0500

    Added recommended concerts to home page

--- front-end/src/components/FavArtistsMini.js
+++ front-end/src/components/FavArtistsMini.js
@@ -28,7 +28,7 @@ const FavArtistsMini = () =>{
     return(
         <div className="favorite-artists">
            <div className="FavoriteArtists">
-           <Link to="/favorite-concerts"> <h3>{demoUser.username}'s' Favorite Artists</h3></Link>
+           <Link to="/favorite-artists"> <h3>{demoUser.username}'s' Favorite Artists</h3></Link>
             <section>
             {favArtists.map(x => (
                 <ArtistComponent key = {x.id} details = {x} />
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,9 +1,58 @@
 import React  from 'react';
+import "./Home.css"
 
 const Home = () => {
+    const recommendedConcerts = [
+        {
+          id: 1,
+          name: 'Example Concert',
+          artist: 'Example Artist',
+          date: 'Example Date',
+          location: 'Example Venue',
+          image: 'https://example.com/image.jpg',
+          link: 'http://localhost:3000/concerts/1',
+        },
+        {
+          id: 2,
+          name: 'Example Concert',
+          artist: 'Example Artist',
+          date: 'Example Date',
+          location: 'Example Venue',
+          image: 'https://example.com/image.jpg',
+          link: 'http://localhost:3000/concerts/2',
+        },
+        {
+          id: 3,
+          name: 'Example Concert',
+          artist: 'Example Artist',
+          date: 'Example Date',
+          location: 'Example Venue',
+          image: 'https://example.com/image.jpg',
+          link: 'http://localhost:3000/concerts/3',
+        },
+    ];
+
     return(
         <div className="Home">
-            <h1> Concerts.io</h1>
+            <div className="home-header">
+                <h1>Concerts.io</h1>
+                <h2>Recommended Concerts</h2>
+            </div>
+            <div className="concerts-container">
+                <div className="recommendedConcerts-container">
+                {recommendedConcerts.map(concert => (
+                    <div key={concert.id} className="recommended-concert">
+                    <img src={concert.imageUrl} alt={concert.artist} />
+                    <div className="concert-details">
+                        <h3>{concert.artist}</h3>
+                        <p>{concert.date}</p>
+                        <p>{concert.location}</p>
+                        <a href={concert.link}>Link</a>
+                    </div>
+                    </div>
+                ))}
+                </div>
+            </div>
         </div>
         
     )
