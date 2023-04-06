8c649a4f4b67fbdc16ce04f62bd921e572a79b9e
joshforlenza <joshforlenza@gmail.com>
2023-04-06 10:08:44 -0400

    Rearranged Spotify API routes

--- back-end/app.js
+++ back-end/app.js
@@ -15,6 +15,8 @@ const app = express();
 
 const SavedConcertsRoute = require('./routes/SavedConcerts');
 const FavoriteArtistsRoute = require('./routes/FavoriteArtists');
+const SpotifyConnectRouter = require('./routes/SpotifyConnnect');
+const SpotifyCallbackRouter = require('./routes/SpotifyCallback');
 
 // Middleware
 
@@ -33,60 +35,8 @@ app.use(cors());
 
 app.use('/SavedConcerts', SavedConcertsRoute);
 app.use('/FavoriteArtists', FavoriteArtistsRoute);
-
-// Routes
-
-// route for connecting spotify account
-app.get('/spotifyconnect', (req, res) => {
-  const state = helpers.generateRandomString(16);
-
-  // requests authorization
-  const scope =
-    'user-top-read user-library-read user-read-private user-read-email user-top-read playlist-modify-private playlist-modify-public';
-  res.redirect(
-    `https://accounts.spotify.com/authorize?${new URLSearchParams({
-      response_type: 'code',
-      client_id: process.env.CLIENT_ID,
-      scope,
-      redirect_uri: process.env.REDIRECT_URI,
-      state,
-    }).toString()}`
-  );
-});
-
-// route user is redirected to after authoring spotify account
-app.get('/spotifycallback', async (req, res) => {
-  // application requests refresh and access tokens
-  // after checking the state parameter
-  const code = req.query.code || null;
-  const state = req.query.state || null;
-
-  if (state === null) {
-    res.redirect(
-      `/#${new URLSearchParams({
-        error: 'state_mismatch',
-      }).toString()}`
-    );
-  } else {
-    const data = await helpers.getToken(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET,
-      code,
-      process.env.REDIRECT_URI
-    );
-    const { access_token } = data;
-    const { refresh_token } = data;
-
-    // get favorite artists
-    const response = await helpers.useAccessToken(
-      'https://api.spotify.com/v1/me/top/artists',
-      access_token
-    );
-    const favArtists = response.items;
-    console.log(favArtists);
-    res.redirect('http://localhost:3001/profile');
-  }
-});
+app.use('/spotifyconnect', SpotifyConnectRouter);
+app.use('/spotifycallback', SpotifyCallbackRouter);
 
 app.get('/login', async (req, res) => {
   try {
