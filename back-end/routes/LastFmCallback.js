57265f069b396ea703b77e36e21861e0e13ea109
joshforlenza <joshforlenza@gmail.com>
2023-04-18 21:38:25 -0400

    Fixed a few errors

--- back-end/routes/LastFmCallback.js
+++ back-end/routes/LastFmCallback.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const passport = require('passport');
 const mongoose = require('mongoose');
 const User = require('../models/User.js');
-const Artist = require('../models/Artist.js')
+const Artist = require('../models/Artist.js');
 const { getSession, getSignature } = require('../lastfmHelperFunctions');
 
 const LastFmCallbackRouter = express.Router();
@@ -22,7 +22,7 @@ LastFmCallbackRouter.get('/', async (req, res, next) => {
     const signature = getSignature(token);
     const session = await getSession(token, signature);
     const sessionKey = session.session.key;
-    
+
     // get favorite artists
     const response = await axios('http://ws.audioscrobbler.com/2.0/', {
       params: {
@@ -32,20 +32,18 @@ LastFmCallbackRouter.get('/', async (req, res, next) => {
         format: 'json',
       },
     });
-    const topartists = response.data.topartists.artist.slice(0,10)
-    const artistDocuments = topartists.map(artist =>{
-        const value =  new Artist({
-          name: artist.name
-        })
-        value.save()
-        return value
-    })
+    const topartists = response.data.topartists.artist.slice(0, 10);
+    const artistDocuments = topartists.map((artist) => {
+      const value = new Artist({
+        name: artist.name,
+      });
+      value.save();
+      return value;
+    });
 
-
-    const user = await User.findOne({_id:new mongoose.Types.ObjectId(req.query.userid)});
-    user.favoriteArtists = artistDocuments
-    await user.save()
-    
+    const user = await User.findOne({ _id: new mongoose.Types.ObjectId(req.query.userid) });
+    user.favoriteArtists = artistDocuments;
+    await user.save();
 
     res.redirect(`http://localhost:3001/profile?sessionKey=${sessionKey}`);
   }
