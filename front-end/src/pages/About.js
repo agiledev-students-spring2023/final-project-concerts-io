ebbce0b849fc6eb9306e7d1483e553b7eadd80e8
mindyjwu <mindy.wu@nyu.edu>
2023-04-30 15:59:42 -0400

    change about us page info and font styling

--- front-end/src/pages/About.js
+++ front-end/src/pages/About.js
@@ -5,7 +5,7 @@ import "./About.css"
 const About = props => {
     return (
       <div className="About">
-        <h2>Concerts.io</h2>
+        <h2>What is Concerts.io ?</h2>
           <p className="paragraph">
           Concerts.io is a website that shows you nearby concerts based on your music taste. 
           Users will receive weekly newsletter updates of shows happening nearby 
@@ -14,14 +14,14 @@ const About = props => {
           ticket prices, age requirement, and a link to purchase. 
           Users will be able to save shows they want to go to and receive reminders of upcoming shows they have tickets for. 
           </p>
-        <h2>Mission Statement</h2>
+        <h2>Our Mission Statement</h2>
           <p className="paragraph">
           Concerts.io mission is to make the concert-going experience easy for all users, 
           freeing them from the hassle and worry of missing out on their favorite band's show.
           </p>
-        <h2>Contact Emails</h2>
+        <h2>Get in Touch with Us !</h2>
           <p className="paragraph">
-            concert_io@nyu.edu
+            please email concert_io@nyu.edu
           </p>
       </div>
     )
